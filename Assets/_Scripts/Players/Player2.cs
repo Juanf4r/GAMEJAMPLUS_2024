6f7a73fd7437647db03b216cc6f4940669162d70
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-10-20 13:14:34 -0600

    un pequeño bug

--- Assets/_Scripts/Players/Player2.cs
+++ Assets/_Scripts/Players/Player2.cs
@@ -290,6 +290,8 @@ public class Player2 : MonoBehaviour
         }
         else if (moreAtt)
         {
+            audios.clip = sonidoGolpeFuerte;
+            audios.Play();
             timerPowerUP = 0f;
             timeStu = 10f;
             uIPlayer2_Att.SetActive(false);
