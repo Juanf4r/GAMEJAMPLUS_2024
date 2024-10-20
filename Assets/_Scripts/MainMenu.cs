402e5912cda3debd4a64b63f821a536a853cc18f
JuanPabloSaavedraRamirez <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2024-10-19 19:26:26 -0600

    menu contoles

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject musicMenu;
     [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject controles;
     private void Start()
     {
         musicMenu.SetActive(false);
@@ -28,11 +29,19 @@ public class MainMenu : MonoBehaviour
     {
         musicMenu.SetActive(true);
         mainMenu.SetActive(false);
+        controles.SetActive(false);
+    }
+    public void controller()
+    {
+        musicMenu.SetActive(false);
+        mainMenu.SetActive(false);
+        controles.SetActive(true);
     }
     public void back()
     {
         musicMenu.SetActive(false);
         mainMenu.SetActive(true);
+        controles.SetActive(false);
     }
     
 }
