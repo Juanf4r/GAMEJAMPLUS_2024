0bd32295bece7d46151beb7cc3bdd516debac49d
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2025-01-07 01:09:14 -0700

    Localization agregado, falta agregar un csv con la traduccion y escoger los textos a cambiar

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
@@ -6,12 +8,38 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private GameObject musicMenu;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject controles;
+
+    private bool _active = false;
+
     private void Start()
     {
+        int ID = PlayerPrefs.GetInt("LocaleKey",0);
+        ChangeLocale(ID);
+
         musicMenu.SetActive(false);
         mainMenu.SetActive(true);
         controles.SetActive(false);
     }
+
+    public void ChangeLocale(int localeID)
+    {
+        if(_active)
+        {
+            return;
+        }
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    private IEnumerator SetLocale(int localeID)
+    {
+        _active = true;
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        PlayerPrefs.SetInt("LocaleKey",localeID);
+        _active = false;
+    }
+
+
     public void iniciarjuego()
     {
         SceneManager.LoadScene(1);
@@ -21,27 +49,30 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
     public void escenaCreditos()
     {
         SceneManager.LoadScene(2);
     }
+
     public void music()
     {
         musicMenu.SetActive(true);
         mainMenu.SetActive(false);
         controles.SetActive(false);
     }
+
     public void controller()
     {
         musicMenu.SetActive(false);
         mainMenu.SetActive(false);
         controles.SetActive(true);
     }
+
     public void back()
     {
         musicMenu.SetActive(false);
         mainMenu.SetActive(true);
         controles.SetActive(false);
-    }
-    
+    }  
 }
