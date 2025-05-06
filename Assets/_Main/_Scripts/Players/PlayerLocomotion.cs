656a40a157132935bbe60f3701b1ede555ee7914
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2025-05-06 13:47:27 -0600

    shadow pixel of player

--- Assets/_Main/_Scripts/Players/PlayerLocomotion.cs
+++ Assets/_Main/_Scripts/Players/PlayerLocomotion.cs
@@ -29,19 +29,33 @@ namespace _Scripts.Players
                 _playerManager.moveVelocity = 0;
                 return;
             }
+
             var targetVelocity = new Vector3(_playerManager.playerInput.x, 0f, _playerManager.playerInput.y).normalized * _playerManager.playerConfig.speed;
+
+            if (targetVelocity.x > 0.01f)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
+            else if (targetVelocity.x < -0.01f)
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+
             _currentVelocity = Vector3.Lerp(_currentVelocity, targetVelocity, Time.deltaTime / _playerManager.playerConfig.accelerationTime);
+
             if (_characterController.isGrounded)
             {
-                _verticalVelocity = 0f; 
+                _verticalVelocity = 0f;
             }
             else
             {
                 _verticalVelocity += Gravity * Time.deltaTime;
             }
+
             var movement = _currentVelocity + Vector3.up * _verticalVelocity;
             _characterController.Move(movement * Time.deltaTime);
             _playerManager.moveVelocity = _characterController.velocity.magnitude;
         }
+
     }
 }
